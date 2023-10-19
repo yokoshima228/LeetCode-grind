42f0c240cb6001f75a1098bd6fa5cdafe201e745
yokoshima228 <145563827+yokoshima228@users.noreply.github.com>
2023-10-19 10:29:51 +0800

    Create Valid Anagram.cs

---
+++ Valid Anagram.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.Design;
+
+public class Program
+{
+    public bool IsAnagram(string s, string t)
+    {
+        if(s.Length != t.Length)
+            return false;
+        char[] arrOfS = s.ToCharArray();
+        char[] arrOfT = t.ToCharArray();
+        Array.Sort(arrOfS);Array.Sort(arrOfT);
+        return arrOfS.SequenceEqual(arrOfT);
+    }
+}
