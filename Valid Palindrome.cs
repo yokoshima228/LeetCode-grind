80c82fd00668b48d6e51041c3c3a2c9dd7796ed4
yokoshima228 <145563827+yokoshima228@users.noreply.github.com>
2023-10-19 10:55:15 +0800

    Create Valid Palindrome.cs

---
+++ Valid Palindrome.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.Design;
+using System.Text;
+
+public class Program
+{
+    public bool IsPalindrome(string s)
+    {
+        StringBuilder newWord = new StringBuilder();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (char.IsLetterOrDigit(s[i]))
+                newWord.Append(char.ToLower(s[i]));
+        }
+
+        string reversedWord = new string(newWord.ToString().Reverse().ToArray());
+
+        return reversedWord == newWord.ToString();
+    }
+}
