18f78abb135a659eea016f24639652ed82b10837
yokoshima228 <145563827+yokoshima228@users.noreply.github.com>
2023-10-20 21:04:55 +0800

    Create Majority Element.cs

---
+++ Majority Element.cs
@@ -0,0 +1,31 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+    public int MajorityElement(int[] nums)
+    {
+        var list = new Dictionary<int, int>() { };
+        foreach (var num in nums)
+        {
+            if (!list.ContainsKey(num))
+            {
+                list[num] = 1;
+            }
+            else
+            {
+                list[num]++;
+            }
+        }
+
+        foreach (var item in list)
+        {
+            if (item.Value > nums.Length / 2)
+            {
+                return item.Key;
+            }
+        }
+        return -1;
+    }
+}
